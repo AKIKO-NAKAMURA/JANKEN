ab9bbfba70866d1485a7b7b02c7c75686e95577e
anpan-man <a22ko3939@gmail.com>
2022-04-20 23:14:36 +0900

    ヘッダーアニメーション

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 "use strict";
+     // 音楽が鳴るボタン
+   const play_btn = document.querySelector('#play-btn');
+      let sound = new Audio("/music/anpan.mp3");
+      play_btn.addEventListener( 'click' , play );
+      function play(){
+      sound.play();
+      }
+
 
  {
   class GameMaster{ //ゲームの進行用のクラス
@@ -50,94 +58,78 @@
     }
   }
 
-  class Opponent{ //相手のクラス
-    constructor(){
-      this._img=document.createElement("img");
+  class Opponent { //相手の手
+    constructor() {
+      this._img = document.createElement("img");
 
-      this._rdNum=Math.floor(Math.random()*JANKEN_TYPE.length);
-      this._img.src="img/"+JANKEN_TYPE[this._rdNum]+".jpg";
+      this._rdNum = Math.floor(Math.random() * JANKEN_TYPE.length);
+      this._img.src = "img/" + JANKEN_TYPE[this._rdNum] + ".jpg";
       DEALER_TAG.appendChild(this._img);
 
       this._timeoutId;
     }
 
-    shuffleImage(){ //ランダムにじゃんけん画像ソースの入れかえ
-      this._rdNum=Math.floor(Math.random()*JANKEN_TYPE.length);
-      this._img.src="img/"+JANKEN_TYPE[this._rdNum]+".jpg";
-
-      this._timeoutId=setTimeout(()=>{
+    shuffleImage() { //ランダムにじゃんけん画像ソースの入れかえ
+      this._rdNum = Math.floor(Math.random() * JANKEN_TYPE.length);
+      this._img.src = "img/" + JANKEN_TYPE[this._rdNum] + ".jpg";
+      this._timeoutId = setTimeout(() => {
         this.shuffleImage();
-      },20); //50fps
+      }, 20); //50fps
     }
 
     result(clickType) { //じゃんけんの結果判定
+       let _dealerType = JANKEN_TYPE[this._rdNum];
+        //クリックすると音が流れる
+      
+        let sound = new Audio("/music/pon2.mp3");
+        sound.play();
+       
 
-      // クリックすると音楽を流す
-      let sound = new Audio("/music/pon2.mp3");
-      sound.play();
-      let _dealerType = JANKEN_TYPE[this._rdNum];
+        switch (clickType) {
+          case "rock": //グーをクリックした場合
+            if (_dealerType === "rock") {
+              RESULT.textContent = "あいこ";
+            } else if (_dealerType === "scissors") {
+              RESULT.textContent = "負け！";
+            } else if (_dealerType === "paper") {
+              RESULT.textContent = "勝ち";
+            }
+            break;
 
-      switch(clickType){
-        case "rock": //グーをクリックした場合
-          if(_dealerType === "rock"){
-            RESULT.textContent="あいこ";
-          }else if(_dealerType === "scissors"){
-            RESULT.textContent="勝ち！";
-          }else if(_dealerType === "paper"){
-            RESULT.textContent="負け";
-          }
-          break;
+          case "scissors": //チョキをクリックした場合
+            if (_dealerType === "rock") {
+              RESULT.textContent = "勝ち";
+            } else if (_dealerType === "scissors") {
+              RESULT.textContent = "あいこ";
+            } else if (_dealerType === "paper") {
+              RESULT.textContent = "負け";
+            }
+            break;
 
-        case "scissors": //チョキをクリックした場合
-          if(_dealerType === "rock"){
-            RESULT.textContent="あいこ";
-          }else if(_dealerType === "scissors"){
-            RESULT.textContent="引き分け";
-          }else if(_dealerType === "paper"){
-            RESULT.textContent="勝ち！";
-          }
-          break;
-
-        case "paper": //パーをクリックした場合
-          if(_dealerType === "rock"){
-            RESULT.textContent="勝ち！";
-          }else if(_dealerType === "scissors"){
-            RESULT.textContent="負け";
-          }else　if(_dealerType === "paper"){
-            RESULT.textContent="あいこ";
-          }
-          break;
-
-        default:
-          console.log("タイプなし");
-          break;
-
+          case "paper": //パーをクリックした場合
+            if (_dealerType === "rock") {
+              RESULT.textContent = "負け";
+            } else if (_dealerType === "scissors") {
+              RESULT.textContent = "勝ち！";
+            } else if (_dealerType === "paper") {
+              RESULT.textContent = "あいこ";
+            }
+            break;
+          default:
+            console.log("タイプなし");
+            break;
+            // if (RESULT = "勝ち"); < statement > window.open('https://www.anpanman.jp/', '_blank'); </statement> 
+        }
       }
     }
-  }
   
-  
-  // 音楽が鳴るボタン
-//play button
-const play_btn = document.querySelector('#play-btn');
 
-//audio file
-  let sound = new Audio("/music/anpan.mp3");
-
-//play event
-play_btn.addEventListener( 'click' , play );
-
-function play(){
-	sound.play();
-}
-
-  
-  const DEALER_TAG=document.querySelector(".dealer"); //タグの取得
+  const DEALER_TAG=document.querySelector(".dealer"); //タ グの取得
   const PLAYER_TAG=document.querySelector(".player"); //タグの取得
   const RESET_BUTTON=document.getElementById("resetButton"); //タグの取得
   const RESULT=document.querySelector(".result"); //タグの取得
   const JANKEN_TYPE=["rock","scissors","paper"]; //じゃんけんタイプの配列
+  const gameMaster = new GameMaster(); //ゲームの進行用のクラスのインスタンスの生成
 
-  const gameMaster=new GameMaster(); //ゲームの進行用のクラスのインスタンスの生成
 }
   
